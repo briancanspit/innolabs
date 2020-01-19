6554b8138e309c796090510b8ca3d3f91e49463d
briancanspit <briancanspit@gmail.com>
2020-01-19 18:21:42 +0300

    Improved contact page with image and labels for input

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -1,36 +1,94 @@
 import React from "react"
-import styled from "styled-components"
-import {Fade} from "react-reveal"
+import styled, { keyframes } from "styled-components"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
+import { Fade } from "react-reveal"
 
-import { Section } from "../global"
+import { Container } from "../global"
 
-const Contact = () => (
-  <Section id="contact" style={{marginTop: -150}}>
-    <StyledContainer>
-      <Fade duration={300} bottom><Subtitle>Contact Us</Subtitle></Fade>
-      <Fade duration={300} top><SectionTitle>We'd love to hear from you</SectionTitle></Fade>
-      <Fade duration={800} clear>
-      <HeaderForm>
-            <HeaderInput placeholder="Full name" />
-            <HeaderInput placeholder="Email address" />
-            <TextAreaInput placeholder="Your message" />
-            <HeaderButton>Send Message</HeaderButton>
-        </HeaderForm>
+const Contact = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(sourceInstanceName: { eq: "product" }, name: { eq: "message" }) {
+        childImageSharp {
+          fluid(maxWidth: 1000) {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <HeaderWrapper id="contact">
+      <Container>
+      <TopContainer>
+      <Fade duration={300} bottom>
+      <Subtitle>Contact Us</Subtitle>
       </Fade>
-    </StyledContainer>
-  </Section>
-)
+      <Fade duration={300} top>
+      <SectionTitle>We would love to hear from you</SectionTitle>
+      </Fade>
+      </TopContainer>
+        <Flex>
+          <Fade duration={300} left>
+            <HeaderForm>
+              <Label>What's your full name?</Label>
+              <HeaderInput placeholder="Full name" />
+              <Label>How can we reach you?</Label>
+              <HeaderInput placeholder="Email address" />
+              <Label>Leave us a message.</Label>
+              <TextAreaInput placeholder="Your message" />
+              <HeaderButton>Send Message</HeaderButton>
+            </HeaderForm>
+          </Fade>
+          <Fade duration={300} right>
+            <Text>
+              {data.file.childImageSharp !== null ? (
+                <Float>
+                  <StyledImage fluid={data.file.childImageSharp.fluid} />
+                </Float>
+              ) : (
+                <span></span>
+              )}
+              <br />
+            </Text>
+          </Fade>
+        </Flex>
+      </Container>
+    </HeaderWrapper>
+  )
+}
 
 export default Contact
 
-const StyledContainer = styled(Section)`
+const HeaderWrapper = styled.header`
   background-color: ${props => props.theme.color.background.light};
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 40px 0px 120px;
+  padding: 225px 0 250px 0;
+  position: relative;
   clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 5vw));
+  @media (max-width: ${props => props.theme.screen.md}) {
+  }
+`
+
+const floating = keyframes`
+  0% {
+    transform: translateY(0px);
+  }
+  50% {
+    transform: translateY(6px);
+  }
+  100%{
+    transform: translateY(0px);
+  }
+`
+
+const Float = styled.div`
+  animation: ${floating} 3.5s linear infinite;
+`
+
+const TopContainer = styled.div`
+  margin-top: -150px;
 `
 
 const SectionTitle = styled.h3`
@@ -49,11 +107,54 @@ const Subtitle = styled.h5`
   text-align: center;
 `
 
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-content: center;
+  margin-top: 100px;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    display: grid;
+    justify-content: space-between;
+    align-content: center;
+    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr;
+    grid-gap: 64px;
+  }
+`
+
+const Label = styled.label`
+  color: #333;
+  margin-left: 2.5px;
+  margin-top: 2.5px;
+`
+
+const Text = styled.div`
+  justify-self: end;
+  align-self: center;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    justify-self: center;
+  }
+`
+
+const StyledImage = styled(Img)`
+  margin-top: -40px;
+  width: 500px;
+  @media (max-width: ${props => props.theme.screen.md}) {
+    width: 400px;
+  }
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    width: 300px;
+    display: none;
+  }
+`
+
 const HeaderForm = styled.form`
   display: flex;
   flex-direction: column;
   padding-bottom: 16px;
   width: 400px;
+  margin-top: -60px;
   @media (max-width: ${props => props.theme.screen.sm}) {
     flex-direction: column;
     width: 80vw;
